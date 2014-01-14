06761edfca4444b792739ca65aca26d87d161eb8
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-01-14 23:52:23 +0000

    Le début du squelette
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8070 6150db3a-0392-43c0-ab3a-2e9def4689b5

---
+++ lang/spip_fr.php
@@ -0,0 +1,10 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://trac.rezo.net/spip/spip/ecrire/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'info_rechercher_02' => 'Rechercher sur le site',
+);
+
+?>
