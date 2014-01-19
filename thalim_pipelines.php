2bb3b4053450981da23a831c9a61d490092bc237
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-01-19 21:55:37 +0000

    Avoir un content/article-evenement.html sur les articles avec au moins un évènement
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8090 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- thalim_pipelines.php
+++ thalim_pipelines.php
@@ -1,4 +1,27 @@
 <?php
 
+if (!defined('_ECRIRE_INC_VERSION')) return;
 
+/**
+ * Insertion dans le pipeline recuperer_fond (SPIP)
+ * 
+ * Si on est dans un article séminaire, que l'on utilise un squelette basé sur z ou zcore, 
+ * on passe la composition seminaire à structure.html
+ * 
+ * Cela permet par exemple d'avoir un squelette content/article-seminaire.html avec spipr
+ */
+function thalim_skel_recuperer_fond($flux){
+	if(isset($flux['args']['contexte']['id_article']) && isset($flux['args']['contexte']['type-page'])
+		&& ($flux['args']['fond'] == 'structure')
+		&& ($flux['args']['contexte']['id_article'] > 0)
+		&& $flux['args']['contexte']['type-page'] == 'article'
+		&& sql_getfetsel('id_evenement','spip_evenements','id_article='.intval($flux['args']['contexte']['id_article']))
+		&& sql_getfetsel('seminaire','spip_articles','id_article='.intval($flux['args']['contexte']['id_article'])) != 'on'){
+			spip_log('evenement','test.'._LOG_ERREUR);
+			spip_log($flux['args']['contexte']['composition'],'test.'._LOG_ERREUR);
+			$flux['args']['contexte']['composition'] = 'evenement';
+			$flux['data'] = evaluer_fond('structure', $flux['args']['contexte']);
+	}
+	return $flux;
+}
 ?>
