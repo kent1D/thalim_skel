fe1aa2a965635e8a36b6295b6f6cee692a277f45
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-04-09 21:46:31 +0000

    Si publié, rediriger vers la page en question
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8152 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- thalim_skel_options.php
+++ thalim_skel_options.php
@@ -7,4 +7,6 @@ $GLOBALS['table_des_traitements']['ROLE'][]= "thalim_role_chaine(%s)";
 define('_COPIE_LOCALE_MAX_SIZE',1002097152);
 if(!test_espace_prive())
 	define('_MAILCRYPT_FONCTION_JS_LANCER_LIEN','thalim_email');
+
+define('_DIOGENE_REDIRIGE_PUBLICATION','oui');
 ?>
