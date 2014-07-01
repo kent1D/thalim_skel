d13cd4f651e5af3963c592a3379dce226e97c8a3
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-07-01 22:58:26 +0000

    Menu déroulant
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8168 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- javascript/thalim.js
+++ javascript/thalim.js
@@ -2,3 +2,10 @@ function thalim_email(a,b){
 	x = "spip.php?page=auteur_email&a="+a+"&b="+b;
 	return x;
 }
+
+$(document).ready(function(){
+	$('li.type_publication').hover(function(){
+		if($(this).find('ul').is(':hidden'))
+			$(this).find('ul').slideDown();
+	});
+});
