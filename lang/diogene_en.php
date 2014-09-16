ac2b86d2dd31f77e8dea45fae47cda491a0466bf
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-16 21:22:10 +0000

    Renommage de chaînes de langue
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8177 6150db3a-0392-43c0-ab3a-2e9def4689b5

---
+++ lang/diogene_en.php
@@ -0,0 +1,9 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'titre_publier_nouveau_page' => 'A new event'
+);
+
+?>
