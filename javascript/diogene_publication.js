6fb5cab8dc2233efd42982595906b73cbab8ded7
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-16 21:24:23 +0000

    Des javascripts pour modifier dynamiquement les formulaires
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8178 6150db3a-0392-43c0-ab3a-2e9def4689b5

---
+++ javascript/diogene_publication.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+	jQuery('#titre').attr('placeholder','');
+	jQuery(".editer_groupe_11").detach().insertBefore('.editer_surtitre');
+	jQuery(".editer_surtitre label").text('Titre de l’article ou du chapitre');
+	jQuery(".editer_titre label").text('Titre de l’ouvrage ou de la revue');
+	jQuery("#diogene_auteurs").detach().insertAfter('.editer_titre');
+	jQuery("#diogene_auteurs .editer_diogene_gerer_auteurs label").text('Auteur(s) Thalim');
+	jQuery(".editer_editeur,.editer_autres_auteurs").detach().insertAfter('#diogene_auteurs');
+	jQuery(".editer_direction_ouvrage").detach().insertAfter('.editer_autres_auteurs');
+	jQuery(".editer_diogene_gerer_auteurs .explication,.editer_liens_sites legend,.editer_liens_sites h3,.editer_nom_site,.editer_langue,#diogene_auteurs legend,#diogene_auteurs h3").remove();
+	jQuery(".editer_url_site label").text('Lien sur une page de site (en savoir plus)');
+	jQuery(".editer_date_orig").detach().insertBefore('.editer_texte');
+	jQuery(".editer_texte label").text('Texte de présentation');
+	jQuery(".editer_date_orig label").text("Date de publication de l'ouvrage ou de l'article");
+	jQuery(".diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
+});
---
+++ javascript/diogene_seminaire.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+	jQuery('#titre').attr('placeholder','');
+	jQuery(".editer_groupe_3").detach().insertBefore('.editer_titre');
+	jQuery("#diogene_auteurs").detach().insertAfter('.editer_titre');
+	jQuery("#diogene_auteurs .editer_diogene_gerer_auteurs label").text('Organisateur(s) Thalim');
+	jQuery(".editer_autres_auteurs label").text('Autre(s) organisateur(s)');
+	jQuery(".editer_autres_auteurs").detach().insertAfter('#diogene_auteurs');
+	jQuery(".editer_diogene_gerer_auteurs .explication,.editer_liens_sites legend,.editer_liens_sites h3,.editer_nom_site,.editer_langue,#diogene_auteurs legend,#diogene_auteurs h3").remove();
+	jQuery(".editer_url_site label").text('Lien sur une page de site (en savoir plus)');
+	jQuery(".editer_texte label").text('Texte de présentation');
+	jQuery(".diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
+});
