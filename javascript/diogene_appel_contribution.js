d0ef83d2c88de213c799246fb2620033e695adaf
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-16 22:16:34 +0000

    Le js pour l'appel à contribution
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8194 6150db3a-0392-43c0-ab3a-2e9def4689b5

---
+++ javascript/diogene_appel_contribution.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function(){
+	jQuery('#titre').attr('placeholder','');
+	jQuery("#diogene_auteurs").detach().insertAfter('.editer_titre');
+	jQuery("#diogene_auteurs label").text('Auteurs/organisateurs');
+	jQuery(".editer_autres_auteurs label").text('Autre(s) auteur(s)/organisateur(s)');
+	jQuery(".editer_autres_auteurs").detach().insertAfter('#diogene_auteurs');
+	jQuery(".editer_url_site label").text("Lien pour plus d'informations sur l'appel");
+	jQuery(".editer_diogene_gerer_auteurs .explication,.editer_liens_sites legend,.editer_liens_sites h3,.editer_nom_site,.editer_langue,#diogene_auteurs legend,#diogene_auteurs h3").remove();
+});
