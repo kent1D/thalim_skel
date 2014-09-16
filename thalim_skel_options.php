b1e11fdbfd4971bffe7b40ab73bc9da1a1b37e1a
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-16 21:45:21 +0000

    Seuls les admins ont accès à ecrire/
    On met le cookie sur chaque auteur dès le login
    
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8180 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- thalim_skel_options.php
+++ thalim_skel_options.php
@@ -9,4 +9,15 @@ if(!test_espace_prive())
 	define('_MAILCRYPT_FONCTION_JS_LANCER_LIEN','thalim_email');
 
 define('_DIOGENE_REDIRIGE_PUBLICATION','oui');
+
+if(isset($GLOBALS['visiteur_session']) && isset($GLOBALS['visiteur_session']['statut']) && in_array($GLOBALS['visiteur_session']['statut'],array('0minirezo','1comite')) && !isset($_COOKIE['spip_admin'])){
+	$activer_cookie = charger_fonction('cookie','action');
+	$activer_cookie("@".$GLOBALS['visiteur_session']['login']);
+}
+
+if(!function_exists('autoriser_ecrire')){
+	function autoriser_ecrire($faire, $type, $id, $qui, $opt) {
+		return ($qui['statut'] =='0minirezo');
+	}
+}
 ?>
