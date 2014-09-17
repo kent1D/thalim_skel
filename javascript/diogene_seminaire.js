8666f1cc1260e404a9bfcd0ab7bad9c55d7ca56a
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-17 22:15:35 +0000

    Petites améliorations
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8198 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- javascript/diogene_seminaire.js
+++ javascript/diogene_seminaire.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function(){
-	jQuery('#titre').attr('placeholder','');
-	jQuery(".editer_groupe_3").detach().insertBefore('.editer_titre');
-	jQuery("#diogene_auteurs").detach().insertAfter('.editer_titre');
-	jQuery("#diogene_auteurs .editer_diogene_gerer_auteurs label").text('Organisateur(s) Thalim');
-	jQuery(".editer_autres_auteurs label").text('Autre(s) organisateur(s)');
-	jQuery(".editer_autres_auteurs").detach().insertAfter('#diogene_auteurs');
-	jQuery(".editer_diogene_gerer_auteurs .explication,.editer_liens_sites legend,.editer_liens_sites h3,.editer_nom_site,.editer_langue,#diogene_auteurs legend,#diogene_auteurs h3").remove();
-	jQuery(".editer_url_site label").text("Lien pour plus d'informations sur le séminaire");
-	jQuery(".editer_texte label").text('Texte de présentation');
-	jQuery(".diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
+	jQuery('.evenements_lies h2').text('Séance(s) du séminaire');
+	jQuery('.formulaire_editer_article #titre').attr('placeholder','');
+	jQuery(".formulaire_editer_article .editer_groupe_3").detach().insertBefore('.formulaire_editer_article .editer_titre');
+	jQuery(".formulaire_editer_article #diogene_auteurs").detach().insertAfter('.formulaire_editer_article .editer_titre');
+	jQuery(".formulaire_editer_article #diogene_auteurs .editer_diogene_gerer_auteurs label").text('Organisateur(s) Thalim');
+	jQuery(".formulaire_editer_article .editer_autres_auteurs label").text('Autre(s) organisateur(s)');
+	jQuery(".formulaire_editer_article .editer_autres_auteurs").detach().insertAfter('.formulaire_editer_article #diogene_auteurs');
+	jQuery(".formulaire_editer_evenement .editer_inscription,.formulaire_editer_evenement .editer_parent,.formulaire_editer_evenement .champs_extras,.formulaire_editer_evenement .editer_repetitions,.formulaire_editer_article .editer_diogene_gerer_auteurs .explication,.formulaire_editer_article .editer_liens_sites legend,.formulaire_editer_article .editer_liens_sites h3,.formulaire_editer_article .editer_nom_site,.editer_langue,.formulaire_editer_article #diogene_auteurs legend,#diogene_auteurs h3").remove();
+	jQuery(".formulaire_editer_article .editer_url_site label").text("Lien pour plus d'informations sur le séminaire");
+	jQuery(".formulaire_editer_article .editer_texte label").text('Texte de présentation');
+	jQuery(".formulaire_editer_article .diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
 });
