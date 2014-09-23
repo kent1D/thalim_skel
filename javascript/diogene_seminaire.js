21efadf1bfa6297203c03e5ebb7736af7483cfee
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-23 21:54:14 +0000

    Amélioration des formulaires
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8199 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- javascript/diogene_seminaire.js
+++ javascript/diogene_seminaire.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function(){
+	jQuery('#titre').attr('placeholder','');
 	jQuery('.evenements_lies h2').text('Séance(s) du séminaire');
 	jQuery('.formulaire_editer_article #titre').attr('placeholder','');
+	jQuery('.formulaire_editer_article .editer_titre label').text('Titre du séminaire');
 	jQuery(".formulaire_editer_article .editer_groupe_3").detach().insertBefore('.formulaire_editer_article .editer_titre');
 	jQuery(".formulaire_editer_article #diogene_auteurs").detach().insertAfter('.formulaire_editer_article .editer_titre');
 	jQuery(".formulaire_editer_article #diogene_auteurs .editer_diogene_gerer_auteurs label").text('Organisateur(s) Thalim');
@@ -10,4 +12,7 @@ jQuery(document).ready(function(){
 	jQuery(".formulaire_editer_article .editer_url_site label").text("Lien pour plus d'informations sur le séminaire");
 	jQuery(".formulaire_editer_article .editer_texte label").text('Texte de présentation');
 	jQuery(".formulaire_editer_article .diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
+	jQuery(".agenda_fieldset h3.legend").text('Ajouter une séance au séminaire');
+	jQuery(".agenda_fieldset .editer_titre_evenement label").text('Titre de la séance');
+	jQuery(".agenda_fieldset .editer_descriptif_evenement label").text('Descriptif de la séance');
 });
--- lang/diogene_fr.php
+++ lang/diogene_fr.php
@@ -3,7 +3,8 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-	'titre_publier_nouveau_page' => 'Un nouvel évènement'
+	'publier_titre' => 'Publier ',
+	'titre_publier_nouveau_page' => ' '
 );
 
 ?>
