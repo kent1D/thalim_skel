938a4be0acae2f7dae54311a846a6a2f1763ec7c
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-30 08:05:59 +0000

    Amélioration du formulaire de séminaire
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8200 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- javascript/diogene_seminaire.js
+++ javascript/diogene_seminaire.js
@@ -1,18 +1,33 @@
 jQuery(document).ready(function(){
 	jQuery('#titre').attr('placeholder','');
-	jQuery('.evenements_lies h2').text('Séance(s) du séminaire');
-	jQuery('.formulaire_editer_article #titre').attr('placeholder','');
-	jQuery('.formulaire_editer_article .editer_titre label').text('Titre du séminaire');
-	jQuery(".formulaire_editer_article .editer_groupe_3").detach().insertBefore('.formulaire_editer_article .editer_titre');
-	jQuery(".formulaire_editer_article #diogene_auteurs").detach().insertAfter('.formulaire_editer_article .editer_titre');
-	jQuery(".formulaire_editer_article #diogene_auteurs .editer_diogene_gerer_auteurs label").text('Organisateur(s) Thalim');
-	jQuery(".formulaire_editer_article .editer_autres_auteurs label").text('Autre(s) organisateur(s)');
-	jQuery(".formulaire_editer_article .editer_autres_auteurs").detach().insertAfter('.formulaire_editer_article #diogene_auteurs');
-	jQuery(".formulaire_editer_evenement .editer_inscription,.formulaire_editer_evenement .editer_parent,.formulaire_editer_evenement .champs_extras,.formulaire_editer_evenement .editer_repetitions,.formulaire_editer_article .editer_diogene_gerer_auteurs .explication,.formulaire_editer_article .editer_liens_sites legend,.formulaire_editer_article .editer_liens_sites h3,.formulaire_editer_article .editer_nom_site,.editer_langue,.formulaire_editer_article #diogene_auteurs legend,#diogene_auteurs h3").remove();
-	jQuery(".formulaire_editer_article .editer_url_site label").text("Lien pour plus d'informations sur le séminaire");
-	jQuery(".formulaire_editer_article .editer_texte label").text('Texte de présentation');
-	jQuery(".formulaire_editer_article .diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
-	jQuery(".agenda_fieldset h3.legend").text('Ajouter une séance au séminaire');
-	jQuery(".agenda_fieldset .editer_titre_evenement label").text('Titre de la séance');
-	jQuery(".agenda_fieldset .editer_descriptif_evenement label").text('Descriptif de la séance');
+	var edit_seminaire = function(){
+		jQuery('.evenements_lies h2').text('Séance(s) du séminaire');
+		jQuery('.formulaire_editer_article #titre').attr('placeholder','');
+		jQuery('.formulaire_editer_article .editer_titre label').text('Titre du séminaire');
+		jQuery('.formulaire_editer_evenement .editer_titre label').text('Titre de la séance de séminaire');
+		jQuery('.formulaire_editer_evenement .editer_mot').hide();
+		jQuery(".formulaire_editer_article .editer_groupe_3").detach().insertBefore('.formulaire_editer_article .editer_titre');
+		jQuery(".formulaire_editer_article #diogene_auteurs").detach().insertAfter('.formulaire_editer_article .editer_titre');
+		jQuery(".formulaire_editer_article #diogene_auteurs .editer_diogene_gerer_auteurs label").text('Organisateur(s) Thalim');
+		jQuery(".formulaire_editer_article .editer_autres_auteurs label").text('Autre(s) organisateur(s)');
+		jQuery(".formulaire_editer_article .editer_autres_auteurs").detach().insertAfter('.formulaire_editer_article #diogene_auteurs');
+		jQuery(".formulaire_editer_evenement .editer_inscription,.formulaire_editer_evenement .editer_parent,.formulaire_editer_evenement .champs_extras,.formulaire_editer_evenement .editer_repetitions,.formulaire_editer_article .editer_diogene_gerer_auteurs .explication,.formulaire_editer_article .editer_liens_sites legend,.formulaire_editer_article .editer_liens_sites h3,.formulaire_editer_article .editer_nom_site,.editer_langue,.formulaire_editer_article #diogene_auteurs legend,#diogene_auteurs h3").remove();
+		jQuery(".formulaire_editer_article .editer_url_site label").text("Lien pour plus d'informations sur le séminaire");
+		jQuery(".formulaire_editer_article .editer_texte label").text('Texte de présentation');
+		jQuery(".formulaire_editer_article .diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
+		jQuery(".agenda_fieldset h3.legend").text('Ajouter une séance au séminaire');
+		jQuery(".agenda_fieldset .editer_titre_evenement label").text('Titre de la séance');
+		jQuery(".agenda_fieldset .editer_descriptif_evenement label").text('Descriptif de la séance');
+		if(jQuery('.formulaire_editer_evenement').size() > 0){
+			if(jQuery('#id_mot option:selected').val() == '')
+				jQuery('#id_mot option[value=476]').attr('selected','selected');
+		}
+		jQuery('.formulaire_editer_evenement form').on('submit',function(){
+			console.log('test');
+			if(jQuery('#id_mot option:selected').val() == '')
+				jQuery('#id_mot option[value=476]').attr('selected','selected');
+		});
+	}
+	edit_seminaire();
+	onAjaxLoad(edit_seminaire);
 });
