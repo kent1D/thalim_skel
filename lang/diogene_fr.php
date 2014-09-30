bf1a8e0404fa3c3bd28e4efc86213486147373c1
kent1 <kent1@6150db3a-0392-43c0-ab3a-2e9def4689b5>
2014-09-30 08:22:21 +0000

    chaines de langue
    git-svn-id: http://svn.aires-de-confluxence.info/svn/squelettes_spip/thalim@8209 6150db3a-0392-43c0-ab3a-2e9def4689b5

--- lang/diogene_fr.php
+++ lang/diogene_fr.php
@@ -3,6 +3,16 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	'texte_statut_en_cours_publie_normal' => 'Publié',
+	'texte_statut_publie_normal' => 'Publié',
+	'texte_statut_en_cours_redaction' => 'En cours de rédaction',
+	'texte_statut_en_cours_poubelle_normal' => 'À la poubelle',
+	'texte_statut_poubelle_normal' => 'À la poubelle',
+	'texte_statut_redaction' => 'En cours de rédaction',
+	'texte_statut_redaction_sans_statut' => 'En cours de rédaction',
+	'label_change_statut_normal' => 'Modifier le statut',
+	'texte_statut_prop_normal' => 'Proposé à la publication',
+	'texte_statut_en_cours_prop_normal' => 'Proposé à la publication',
 	'publier_titre' => 'Publier ',
 	'titre_publier_nouveau_page' => ' '
 );
