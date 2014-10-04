0426cedc8dec23f02201c0cefa8807cfc0c6a375
kent1 <kent1@arscenic.info>
2014-10-04 19:10:42 +0200

    Les infos du paquet.xml

---
+++ lang/paquet-thalim_skel_fr.php
@@ -0,0 +1,13 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_plugins_/trad-lang/trunk/lang/
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'thalim_skel_description' => 'Le squelette utilisé pour l\'unité mixte de recherche [Thalim->http://www.thalim.cnrs.fr].',
+	'thalim_skel_slogan' => 'Squelette de l\'unité mixte de recherche Thalim'
+);
+
+?>
