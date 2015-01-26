5c7de48028076ee876e19acf8347c71a6f54aba3
kent1 <kent1@arscenic.info>
2015-01-26 23:19:42 +0100

    date de production considérée comme date de publication

--- lang/hals_publication_fr.php
+++ lang/hals_publication_fr.php
@@ -3,6 +3,10 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// L
+	'label_date_production' => 'Date de publication',
+	
+	// T
 	'typdoc_art' => 'Article',
 );
 
