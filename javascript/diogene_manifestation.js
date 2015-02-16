f3c69378a770d99d16adc0ed381f1ce72e63054b
kent1 <kent1@arscenic.info>
2015-02-16 23:39:35 +0100

    Le js du nouveau formulaire

---
+++ javascript/diogene_manifestation.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function(){
+	jQuery('#titre').attr('placeholder','').addClass('inserer_barre_simple');;
+	jQuery(".editer_groupe_3").detach().insertBefore('.editer_titre');
+	jQuery('.editer_titre_evenement').hide();
+	jQuery("#diogene_auteurs").detach().insertAfter('.editer_soustitre');
+	jQuery(".editer_titre label").text('Titre à afficher');
+	jQuery("#diogene_auteurs .editer_diogene_gerer_auteurs label").text('Membre(s) Thalim');
+	jQuery(".editer_autres_auteurs label").text('Autre(s) intervenant(s)');
+	jQuery(".editer_autres_auteurs").detach().insertAfter('#diogene_auteurs');
+	jQuery(".editer_diogene_gerer_auteurs .explication,.editer_liens_sites legend,.editer_liens_sites h3,.editer_nom_site,.editer_langue,#diogene_auteurs legend,#diogene_auteurs h3").remove();
+	jQuery(".editer_url_site label").text("Lien pour plus d'informations sur l'événement");
+	jQuery(".editer_texte label").text('Texte de présentation');
+	jQuery(".diogene_mots legend").text('A relier à (pas obligatoire pour tous)');
+	jQuery(".editer_groupe_17").detach().insertAfter('.editer_diogene_gerer_auteurs');
+	if($('#horaire').is(':checked')){
+		$('#horaire').attr('checked', false);
+		$('.afficher_horaire').show();
+	}
+	jQuery("em.aide").remove();
+	barrebouilles_thalim();
+});
